e496249b400af0360ee6850e2459d0648410f72d
Nam Pham <nampdn@protonmail.com>
2021-09-23 09:25:25 +0700

    refactor(hooks): integrate with typechain snippet

--- frontend/src/hooks/useContract.ts
+++ frontend/src/hooks/useContract.ts
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import useWeb3 from "./useWeb3";
 import { getIbKaiContract, getKaloContract, getMasterchefContract, getErc20Contract } from "utils/contractHelpers";
+// import { TreasuryFactory } from '../some/path/to/generated/typechain';
 
 export const useIbKAI = () => {
   const { library } = useWeb3();
@@ -21,3 +22,16 @@ export const useERC20 = (address: string) => {
   const { library } = useWeb3();
   return useMemo(() => getErc20Contract(address, library.getSigner()), [library]);
 };
+
+/**
+export const useTreasury = (address: string) => {
+  const web3 = useWeb3();
+  return useMemo(() => {
+    const treasuryContract = TreasuryFactory.connect(
+      address,
+      (web3 as unknown) as any
+    );
+    return treasuryContract;
+  }, [address, web3]);
+};
+*/
