9aa94504a0f88baeee324d4f4eb6c9331e158d18
lddang1762 <lddang1762@csu.fullerton.edu>
2021-11-10 17:30:47 -0800

    chore(deploy): deploy masterchef and mockLP for testnet

--- frontend/src/utils/contractHelpers.ts
+++ frontend/src/utils/contractHelpers.ts
@@ -10,9 +10,6 @@ import {
 } from "./addressHelpers";
 
 import erc20Abi from "config/abi/erc20.json";
-import kalomiraAbi from "config/abi/Kalomira.json";
-import ibKaiAbi from "config/abi/ibKAI.json";
-import lpTokenAbi from "config/abi/MockLP.json";
 import MasterChefAbi from "config/abi/MasterChef.json";
 import MulticallAbi from "config/abi/Multicall.json";
 
@@ -23,18 +20,6 @@ const getContract = (abi: any, address: string, signer?: ethers.Signer | ethers.
 
 export const getErc20Contract = (address: string, signer?: ethers.Signer | ethers.providers.Provider) => {
   return getContract(erc20Abi, address, signer);
-};
-
-export const getIbKaiContract = (signer?: ethers.Signer | ethers.providers.Provider) => {
-  return getContract(ibKaiAbi, getIbKAIAddress(), signer);
-};
-
-export const getKaloContract = (signer?: ethers.Signer | ethers.providers.Provider) => {
-  return getContract(kalomiraAbi, getKaloAddress(), signer);
-};
-
-export const getMasterchefContract = (signer?: ethers.Signer | ethers.providers.Provider) => {
-  return getContract(MasterChefAbi, getMasterchefAddress(), signer);
 };
 
 export const getMulticallContract = (signer?: ethers.Signer | ethers.providers.Provider) => {
